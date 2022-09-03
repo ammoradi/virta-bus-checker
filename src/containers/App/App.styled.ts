99991133c72bc200cfb8ac9cf7d59919df4acdbc
Amir Mohamad Moradi <amirmohammad.moradi@gmail.com>
2022-09-03 05:33:08 +0300

    feat: add background image

---
+++ src/containers/App/App.styled.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+import BackgroundImage from 'assets/images/background.png'
+
+export const StyledAppContainer = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  background-image: url(${BackgroundImage});
+  background-size: auto 342px;
+  background-position: bottom right;
+  background-repeat: no-repeat;
+
+  @media only screen and (max-width: 1220px) {
+    background-size: auto 296px;
+  }
+
+  @media only screen and (max-width: 1024px) {
+    background-size: auto 256px;
+  }
+
+  @media only screen and (max-width: 868px) {
+    background-size: auto 216px;
+  }
+
+  @media only screen and (max-width: 700px) {
+    background-size: auto 196px;
+  }
+
+  @media only screen and (max-width: 600px) {
+    background-size: auto 176px;
+  }
+
+  @media only screen and (max-width: 500px) {
+    background-image: none;
+  }
+`
--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import {useStopTimesQuery} from './App.graphql.generated'
 
+import {StyledAppContainer} from './App.styled'
+
 function AppContainer() {
   const {data} = useStopTimesQuery({ fetchPolicy: 'cache-and-network' })
 
   console.log(data)
 
-  return <>Main</>
+  return (
+    <StyledAppContainer>
+      Main
+    </StyledAppContainer>
+  )
 }
 
 export default AppContainer
