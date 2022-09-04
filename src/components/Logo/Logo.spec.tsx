645fb2246899dbe63d081fce558e608e046b4beb
Amir Mohamad Moradi <amirmohammad.moradi@gmail.com>
2022-09-04 22:34:28 +0300

    fix: Logo tests

--- src/components/Logo/Logo.spec.tsx
+++ src/components/Logo/Logo.spec.tsx
@@ -1,19 +1,20 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 
+import { renderWithThemeProvider } from 'utils/testUtils';
 import LogoImage from 'assets/images/logo.png'
 
 import Logo from './Logo'
 
 describe('<Logo />', () => {
   it('should display the correct logo image', () => {
-    render(<Logo />)
+    renderWithThemeProvider(<Logo />)
 
     expect(screen.getByRole('img')).toHaveAttribute('src', LogoImage)
   })
 
   it('should render with the correct alt text', () => {
-    render(<Logo />)
+    renderWithThemeProvider(<Logo />)
 
     expect(screen.getByAltText('Virta Ltd.')).toBeInTheDocument()
   })
